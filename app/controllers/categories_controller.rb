fa51cdc278dc19cd434f4e8d4d2f94f75697f0d3
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-03-27 13:52:10 -0700

    add corrections

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render :index
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @category = Category.create(
+      category_name: params[:category_name],
+    )
+    render :show
+  end
+
+  def update
+    @category = Category.find_by(id: params[:id])
+    @category.update(
+      name: params[:name] || @category.name,
+    )
+    render :show
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    @category.destroy
+    render json: { message: "Item destroyed successfully" }
+  end
+end
---
+++ app/controllers/todo_controller.rb
@@ -0,0 +1,43 @@
+class TodoController < ApplicationController
+  def index
+    @todo = Todo.all
+    render :index
+  end
+
+  def show
+    @todo = Todo.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @todo = Todo.create(
+      name: params[:name],
+    )
+    if @todo.valid?
+      render json: { message: "todo created!" }
+    else
+      render json: { errors: @todo.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @todo = Todo.find_by(id: params[:id])
+    @product.update(
+      name: params[:name] || @todo.name,
+      completed: params[:completed] || @todo.completed,
+      item_id: params[:item_id] || @todo.item_id,
+    )
+    if @todo.valid?
+      render :show
+    else
+      render json: { errors: @todo.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @todo = Todo.find_by(id: params[:id])
+    @todo.destroy
+  end
+end
