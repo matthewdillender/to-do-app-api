28b0f479aed76d794b8f19f6d6e2038a2b8d5758
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-03-27 16:50:07 -0400

    deleted weird controller

--- app/controllers/reminders_controller.rb
+++
@@ -1,5 +0,0 @@
-class RemindersController < ApplicationController
-    def index
-        render json: {message: "U have a unicorn dinner party later tonight"}
-    end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :email, presence: true, uniqueness: true
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 
-    has_many :lists
-    
+  has_many :lists
 end
