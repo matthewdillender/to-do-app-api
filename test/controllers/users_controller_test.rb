b1904bc313ffc1345c37c9adac4118574e991e14
rubygomez <rvbii.gomez@gmail.com>
2024-03-27 13:58:37 -0700

    user controller tests + renamed test controllers

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: { name: "user", email: "email.com", password: "password", password_confirmation: "password confirmation" }
+      assert_response 200
+    end
+  end
 end
