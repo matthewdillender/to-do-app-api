1efbf572147d94952904d01bc3227d77992ddbca
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-03-28 16:35:25 -0400

    tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
-
-
-class SessionsController < ApplicationController
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "create" do
-    assert_difference "Session.count", 1 do
-      post "/sessions.json", params: { email: "email.com", password: "sessionpassword"}
-      assert_response 200
-    end
+    post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    assert_response 201
+
+    data = JSON.parse(response.body)
+    assert_equal ["jwt", "email", "user_id"], data.keys
   end
 end
