3fe5f70d5f8eb960bb26e5bd677b4a10259fef66
rubygomez <rvbii.gomez@gmail.com>
2024-03-28 10:39:45 -0700

    todo controller changes

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+  # test "create" do
+  #   assert_difference "Todo.count", 1 do
+  #     post "/todos.json", params: { name: "todo", completed: false, user_id: 1, category_id: 1, description: "test description", deadline: 1999-12-02}
+  #     assert_response 200
+  #   end
+  # end
+
+  # test "index" do
+  #   get "/todos.json"
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal Todo.count, data.length
+  # end
+
+  # test "show" do
+  #   get "/todos/#{Todo.first.id}.json"
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal ["id", "title", "completed", "user_id", "category_id", "description", "deadline"], data.keys
+  # end
+
+  # test "update" do
+  #   todo = Todo.first
+  #   patch "/todos/#{todo.id}.json", params: { title: "Updated name" }
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal "Updated name", data["name"]
   # end
 end
